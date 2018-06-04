0b51e741190a0a5addab1c19e4b5b5a42f634c43
Jeroen Schmidt <jeroen.f.l.schmidt@gmail.com>
2018-06-04 22:10:25 +0000

    working s3 copy in the tweet ingestion script

--- Projects/projects for submission/capstone/get-tweets/get-tweets-data-ranges.py
+++ Projects/projects for submission/capstone/get-tweets/get-tweets-data-ranges.py
@@ -80,9 +80,61 @@ def main(argv):
             r = random.randint(1,60*10)
             time.sleep(60 + r)
 
+    def move_to_s3(path=etherium/):
+        bucket_name = 'jeroens-bucket'
 
-    print("Finished Date Ranges")
-    print("Starting move to S3")
+        # source directory
+        sourceDir = path
+        # destination directory name (on s3)
+        destDir = ''
+
+        #max size in bytes before uploading in parts. between 1 and 5 GB recommended
+        MAX_SIZE = 20 * 1000 * 1000
+        #size of parts when uploading in parts
+        PART_SIZE = 6 * 1000 * 1000
+
+        conn = boto.connect_s3()
+
+        bucket = conn.get_bucket(bucket_name)
+
+        uploadFileNames = []
+        for (sourceDir, dirname, filename) in os.walk(sourceDir):
+            uploadFileNames.extend(filename)
+            break
+
+        def percent_cb(complete, total):
+            sys.stdout.write('.')
+            sys.stdout.flush()
+
+        for filename in uploadFileNames:
+            sourcepath = os.path.join(sourceDir + filename)
+            destpath = os.path.join(destDir, filename)
+            print 'Uploading %s to Amazon S3 bucket %s' % \
+                (sourcepath, bucket_name)
+
+            filesize = os.path.getsize(sourcepath)
+            if filesize > MAX_SIZE:
+                print "multipart upload"
+                mp = bucket.initiate_multipart_upload(destpath)
+                fp = open(sourcepath,'rb')
+                fp_num = 0
+                while (fp.tell() < filesize):
+                    fp_num += 1
+                    print "uploading part %i" %fp_num
+                    mp.upload_part_from_file(fp, fp_num, cb=percent_cb, num_cb=10, size=PART_SIZE)
+
+                mp.complete_upload()
+
+            else:
+                print "singlepart upload"
+                k = boto.s3.key.Key(bucket)
+                k.key = destpath
+                k.set_contents_from_filename(sourcepath,
+                        cb=percent_cb, num_cb=10)
+
+
+    print("Finished Ingesting All the Tweets for the Given Ranges")
+    print("Starting S3 Copy")
     move_to_s3(path=folder)
 
 
